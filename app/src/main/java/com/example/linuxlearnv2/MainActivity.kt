1c03fe533e95930f6ca21bb2ec2584f3646d6052
dhikaaldhika22 <dhikaaldhika018@gmail.com>
2023-02-26 18:40:49 +0700

    add navigation bottom stuff

--- app/src/main/java/com/example/linuxlearnv2/MainActivity.kt
+++ app/src/main/java/com/example/linuxlearnv2/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.linuxlearnv2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.linuxlearnv2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.navView.setItemSelected(R.id.nav_home, true)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.nav_host_fragment, HomeFragment())
+            .commit()
+
+        supportActionBar?.hide()
+        navBottom()
     }
+
+    private fun navBottom() {
+        binding.navView.setOnItemSelectedListener {
+            var fragment: Fragment?
+            fragment = when (it) {
+                R.id.nav_exercise -> ExerciseFragment()
+                R.id.nav_profile -> ProfileFragment()
+                else -> HomeFragment()
+            }
+
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.nav_host_fragment, fragment)
+                .commit()
+        }
+    }
+
 }
